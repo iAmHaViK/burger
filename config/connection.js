9fd4ded16166dd079256787096a6cffe78d6b711
Hassan Ali <Dotmatrix_fusion@hotmail.com>
2018-03-14 20:43:28 -0700

    adds heroku changes

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	host:'localhost',
-	user:'root',
-	password:'1996',
-	database:'burgers_db'
-})
+var connection;
 
-connection.connect(function(err){
-	if(err)throw err;
-	console.log('connected as id: ' + connection.threadid)
-})
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+			host:'localhost',
+			user:'root',
+			password:'1996',
+			database:'burgers_db'
+	})
+};
 
+connection.connect();
 module.exports = connection;
